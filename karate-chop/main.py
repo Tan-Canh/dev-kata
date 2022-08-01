1f7441fea09d34a24defaf23c9ad18790180ac9a
toddnguyengithub <toddnguyenbusiness@gmail.com>
2022-08-01 09:22:48 +0700

    Commit source

---
+++ karate-chop/main.py
@@ -0,0 +1,25 @@
+def recursive_binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return recursive_binary_search(arr, low, mid - 1, x)
+        else:
+            return recursive_binary_search(arr, mid + 1, high, x)
+ 
+    else:
+        return -1
+def iterative_binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
