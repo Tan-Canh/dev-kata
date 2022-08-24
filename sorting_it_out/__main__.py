eb14410d9a6d16d3682805724ceb8d91051e781e
Phạm Lê Gia Đại <daiplg@fpt.com.vn>
2022-08-24 14:47:50 +0700

    Refactor code:
    - Rename tests
    - Add build_character_map, filter, build_result methods.

--- sorting_it_out/__main__.py
+++ sorting_it_out/__main__.py
@@ -2,17 +2,27 @@ import string
 
 
 def sorting_it_out(value: str, case_sensitive: bool = False) -> str:
-    cdict = {}
-    for char in value:
-        if char not in string.ascii_letters:
-            continue
-        if not case_sensitive:
-            char = char.lower()
-        cdict[char] = cdict[char] + 1 if char in cdict else 1
+    character_map = build_character_map(value, case_sensitive)
+    return build_result_string(character_map, case_sensitive)
 
+
+def build_character_map(value: str, case_sensitive: bool) -> dict:
+    filtered_string = filter_non_alphabet_characters(value)
+    character_map = {}
+    for character in filtered_string:
+        key = character if case_sensitive else character.lower()
+        character_map[key] = 1 if key not in character_map else character_map[key] + 1
+    return character_map
+    
+    
+def filter_non_alphabet_characters(value: str) -> str:
+    return ''.join(filter(str.isalpha, value))
+
+
+def build_result_string(character_map: dict, case_sensitive: bool) -> str:
     result = ""
-    alphabet = string.ascii_letters if case_sensitive else string.ascii_lowercase
-    for key in alphabet:
-        if key in cdict:
-            result += key * cdict[key]
+    keys = string.ascii_letters if case_sensitive else string.ascii_lowercase
+    for key in keys:
+        if key in character_map:
+            result += key * character_map[key]
     return result
--- sorting_it_out/test.py
+++ sorting_it_out/test.py
@@ -4,31 +4,31 @@ from sorting_it_out.__main__ import sorting_it_out
 
 
 class TestSortingItOut(unittest.TestCase):
-    def test_it_should_pass_if_return_empty_result(self):
+    def test_it_should_return_empty_when_input_is_empty(self):
         value = ""
         actual = sorting_it_out(value)
         expect = ""
         self.assertEqual(expect, actual)
 
-    def test_it_should_pass_if_not_contain_any_non_alphabetical_characters(self):
+    def test_it_should_return_alphabet_only_characters(self):
         value = "<,?':.abcABa"
         actual = sorting_it_out(value)
         expect = "aaabbc"
         self.assertEqual(expect, actual)
 
-    def test_it_should_pass_if_not_contain_any_white_space(self):
+    def test_it_should_not_return_any_white_space(self):
         value = " abcAAA"
         actual = sorting_it_out(value)
         expect = "aaaabc"
         self.assertEqual(expect, actual)
 
-    def test_it_should_pass_if_return_correct_value_when_case_sensitive(self):
+    def test_it_should_return_correct_result_when_case_sensitive(self):
         value = "ABCabc"
         actual = sorting_it_out(value, case_sensitive=True)
         expect = "abcABC"
         self.assertEqual(expect, actual)
 
-    def test_it_should_pass_if_return_correct_result(self):
+    def test_it_should_return_correct_result(self):
         value = (
             "When not studying nuclear physics, Bambi likes to play beach volleyball."
         )
